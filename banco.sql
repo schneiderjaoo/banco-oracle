b6e95f1b611653c345ba41d272ae9f72dd6b783b
João Schneider <126828193+schneiderjaoo@users.noreply.github.com>
2023-07-11 09:15:11 -0300

    Update banco.sql
    Funcionários criados e printados em tala

--- banco.sql
+++ banco.sql
@@ -1,20 +1,33 @@
-create table empresa(
+create table empresa(  --criando tabela de empresa--
   idEmpresa int primary key not null,
   nomeEmpresa varchar(100) not null,
   descricaoEmpresa varchar(255)
 );
 
-create table funcionarios(
+create table funcionarios(  --criando tabela funcionarios--
   idEmpresa int not null,
   idFuncionario int primary key not null,
   nomeFuncionario varchar(100) not null,
   salarioFuncionarios float not null 
 );
 
-insert into empresa ( idEmpresa, nomeEmpresa, descricaoEmpresa)
-values ( 1, " Empresa um ", " Empresa número um ");
+insert into empresa ( idEmpresa, nomeEmpresa, descricaoEmpresa)  --criando a empresa número um--
+values ( 1, ' Empresa um ', ' Empresa número um ');
 
-insert into empresa ( idEmpresa, nomeEmpresa, descricaoEmpresa)
-values ( 2, " Empresa dois ", " Empresa número dois ");
+insert into empresa ( idEmpresa, nomeEmpresa, descricaoEmpresa)  --criando a empresa número dois--
+values ( 2, ' Empresa dois ', ' Empresa número dois ');
 
 select * from empresa a -- seleciona a tabela e mostra todas as empresas, "select * from empresa a where idEmpresa = 1", aqui ele puxa a emrpesa com id = 1--
+
+insert into funcionarios( idEmpresa, idFuncionario, nomeFuncionario, salarioFuncionarios ) --inserindo informações de funcionarios da empresa número um--
+values ( 1, 11, 'nome um', 1000);
+insert into funcionarios( idEmpresa, idFuncionario, nomeFuncionario, salarioFuncionarios )
+values ( 1, 12, 'nome dois', 1000);
+
+insert into funcionarios( idEmpresa, idFuncionario, nomeFuncionario, salarioFuncionarios )  --inserindo informações de funcionarios da empresa número dois--
+values ( 2, 21, 'nome um', 1200);
+insert into funcionarios( idEmpresa, idFuncionario, nomeFuncionario, salarioFuncionarios )
+values ( 2, 22, 'nome dois', 1200);
+
+select * from funcionarios a  --puxando dados de funcionários da empresa número um--
+  where idEmpresa = 1
