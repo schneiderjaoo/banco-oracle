68018b835d55d8ea1309814983e4625103a0d6e6
João Schneider <126828193+schneiderjaoo@users.noreply.github.com>
2023-07-17 12:57:33 -0300

    Update processos.java
    Inserido lógica no processos de pagamento, métodos privados para estabelecer a conexão

--- processos.java
+++ processos.java
@@ -5,51 +5,52 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Pagamento {
+    private static final String URL = "jdbc:mysql://localhost:3306/banco";
+    private static final String USERNAME = "usuario";
+    private static final String PASSWORD = "senha";
+
+   private static float calcularPagamento(float salario) {
+    // Lógica de processamento dos pagamentos
+    float taxaImposto = 0.1f; // de 10%
+
+    float valorPagamento = salario * (1 - taxaImposto); // Desconta o imposto
+
+    return valorPagamento;
+}
+
+    }
 
     public static void main(String[] args) {
-        // Acesso ao banco de dados definido no host:porta
-        String url = "jdbc:mysql://localhost:3306/banco";
-        String username = "usuario";
-        String password = "senha";
+        try (Connection connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+             Statement statement = connection.createStatement()) {
 
-        try {
-            // Definindo a conexão
-            Connection connection = DriverManager.getConnection(url, username, password);
-            Statement statement = connection.createStatement();
-
-            // Consulta informações dos pedidos
+            // Consultando informações 
             String sql = "SELECT p.descricaoPedido, e.nomeEmpresa, f.nomeFuncionario, f.salarioFuncionarios " +
                     "FROM pedidos p " +
                     "JOIN processo pr ON p.idProduto = pr.idProduto " +
                     "JOIN empresa e ON pr.idEmpresa = e.idEmpresa " +
                     "JOIN funcionarios f ON e.idEmpresa = f.idEmpresa";
 
-            ResultSet resultSet = statement.executeQuery(sql);
+            try (ResultSet resultSet = statement.executeQuery(sql)) {
+                // Print das informações na tela
+                while (resultSet.next()) {
+                    String descricaoPedido = resultSet.getString("descricaoPedido");
+                    String nomeEmpresa = resultSet.getString("nomeEmpresa");
+                    String nomeFuncionario = resultSet.getString("nomeFuncionario");
+                    float salarioFuncionarios = resultSet.getFloat("salarioFuncionarios");
 
-            // Resultados da consulta
-            while (resultSet.next()) {
-                String descricaoPedido = resultSet.getString("descricaoPedido");
-                String nomeEmpresa = resultSet.getString("nomeEmpresa");
-                String nomeFuncionario = resultSet.getString("nomeFuncionario");
-                float salarioFuncionarios = resultSet.getFloat("salarioFuncionarios");
+                    // Lógica de processamento dos pagamentos
+                    float valorPagamento = calcularPagamento(salarioFuncionarios);
 
-                // Lógica de processamento dos pagamentos
-                float valorPagamento = calcularPagamento(salarioFuncionarios);  // Exemplo de cálculo de pagamento
-
-                // Printa pagamentos
-                System.out.println("Descrição do Pedido: " + descricaoPedido);
-                System.out.println("Empresa: " + nomeEmpresa);
-                System.out.println("Funcionário: " + nomeFuncionario);
-                System.out.println("Salário: " + salarioFuncionarios);
-                System.out.println("Valor do Pagamento: " + valorPagamento);
-                System.out.println("-------------------------------------");
+                    // Printa pagamentos
+                    System.out.println("Descrição do Pedido: " + descricaoPedido);
+                    System.out.println("Empresa: " + nomeEmpresa);
+                    System.out.println("Funcionário: " + nomeFuncionario);
+                    System.out.println("Salário: " + salarioFuncionarios);
+                    System.out.println("Valor do Pagamento: " + valorPagamento);
+                    System.out.println("-------------------------------------");
+                }
             }
-
-            // Fecha conexão ao banco
-            resultSet.close();
-            statement.close();
-            connection.close();
-
         } catch (SQLException e) {
             e.printStackTrace();
         }
